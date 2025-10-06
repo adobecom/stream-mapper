d870a463cff99e4cf31510ace82ea7f4fb6637db
mathuria <mathuria@adobe.com>
2025-10-06 22:11:44 +0530

    Passing section wrapped div in place of single block for adjacent dom additions like metadata

---
+++ streamlibs/utils/constants.js
@@ -0,0 +1,8 @@
+export const ACCENT_BARS = {
+  'Accent Bar / CC Gradient': 'linear-gradient(90deg, #FA0F00 0%, #E9740A 15.42%, #FFCE2E 39.44%, #009C3B 67.99%, #2799F6 85.76%, #6349E0 95.42%, #9999FC 100%)',
+  'Accent Bar / CC Photo': '#31A8FF',
+  'Accent Bar / CC Graphic': '#EB1000',
+  'Accent Bar / CC Video': '#99F',
+  'Accent Bar / CC Illustration': '#FF9A00',
+  'Accent Bar / CC 3D': '#99E83F',
+}
