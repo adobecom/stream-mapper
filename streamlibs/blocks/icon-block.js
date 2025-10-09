d0cc2f03664182f03528f5a55606070aec600476
Saurabh Sircar <saurabhsircar11@gmail.com>
2025-10-09 17:30:30 +0530

    feat: added icon bio block config

--- streamlibs/blocks/icon-block.js
+++ streamlibs/blocks/icon-block.js
@@ -11,6 +11,24 @@ function handleBlockVariants(blockContent, properties) {
   }
 }
 
+function handleAlign(blockContent, value) {
+  if (value === 'center') {
+    blockContent?.classList.add('center');
+  }
+}
+
+function handleIconSize(blockContent, properties, tag, sizeKey) {
+  if (properties?.miloTag?.includes(tag)) {
+    let size = '';
+    const sizeValue = properties?.[sizeKey]?.name?.toLowerCase().trim();
+    if (sizeValue.includes(' m ')) size = 'm';
+    if (sizeValue.includes('l')) size = 'l';
+    if (size) {
+      blockContent?.classList.add(`${size}-icon`);
+    }
+  }
+}
+
 function handleVariants(sectionWrapper, blockContent, properties) {
   handleBlockVariants(blockContent, properties);
   if (properties?.topSpacer) handleSpacer(blockContent, properties.topSpacer.name, 'top');
@@ -18,6 +36,8 @@ function handleVariants(sectionWrapper, blockContent, properties) {
   if (properties?.accentBar?.name) {
     handleAccentBar(sectionWrapper, blockContent, properties.accentBar.name);
   }
+  if (properties?.miloTag?.includes('bio')) handleAlign(blockContent, properties.align);
+  handleIconSize(blockContent, properties, properties?.miloTag, properties?.miloTag?.includes('bio') ? 'bioDetails' : 'productLockup');
 }
 
 function handleProductLockup(value, areaEl) {
@@ -30,6 +50,13 @@ function handleProductLockup(value, areaEl) {
     anchorElement.textContent = productLogo;
   }
 }
+
+function handleAvatar(value, areaEl) {
+  if (!value) return;
+  areaEl.querySelectorAll('source').forEach((source) => { source.srcset = value; });
+  areaEl.querySelector('img').src = value;
+}
+
 export default async function mapBlockContent(
   sectionWrapper,
   blockContent,
@@ -38,7 +65,11 @@ export default async function mapBlockContent(
   const properties = figContent?.details?.properties;
   if (!properties) return;
   try {
-    const mappingData = await safeJsonFetch('icon-block.json');
+    let configJson = 'icon-block.json';
+    if (properties?.miloTag?.includes('bio')) {
+      configJson = 'icon-bio-block.json';
+    }
+    const mappingData = await safeJsonFetch(configJson);
     mappingData.data.forEach((mappingConfig) => {
       const value = properties[mappingConfig.key];
       const areaEl = handleComponents(blockContent, value, mappingConfig);
@@ -48,6 +79,9 @@ export default async function mapBlockContent(
           break;
         case 'actions':
           handleActionButtons(blockContent, properties, value, areaEl);
+          break;
+        case 'bio':
+          handleAvatar(value, areaEl);
           break;
         default:
           break;
