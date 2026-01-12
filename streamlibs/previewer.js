8b8e7f3bc1e54f39cc6768aa0d7ecbe471ba8524
mathuria <mathuria@adobe.com>
2026-01-13 01:18:02 +0530

    Adding loader on push to DA

--- streamlibs/previewer.js
+++ streamlibs/previewer.js
@@ -31,9 +31,9 @@ const LOADER = document.querySelector('#loader-container');
 const EDIT_MAPPER = document.querySelector('#edit-operation-container');
 let BUTTON_CONTAINER = null;
 
-function handleLoader(displayLoader = true) {
+function handleLoader(displayLoader = true, message = null) {
   if (!displayLoader) return;
-  const loaderMessage = LOADER_MSG_LIST[Math.floor(Math.random() * LOADER_MSG_LIST.length)];
+  const loaderMessage = message || LOADER_MSG_LIST[Math.floor(Math.random() * LOADER_MSG_LIST.length)];
   LOADER_MESSAGE_AREA.textContent = loaderMessage;
   LOADER.style.display = 'flex';
   LOADER.classList.add('is-visible');
@@ -249,8 +249,14 @@ export default async function initPreviewer() {
 
 export async function persist() {
   try {
+    handleLoader(true, 'Pushing content to DA');
+    hideDOMElements([document.querySelector('main')]);
     await persistOnTarget();
+    hideDOMElements([LOADER]);
+    showDOMElements([document.querySelector('main')]);
   } catch (error) {
+    hideDOMElements([LOADER]);
+    showDOMElements([document.querySelector('main')]);
     handleError(error, 'persisting content');
     throw error;
   }
