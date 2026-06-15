263a7e10a78001bf3d4bca76d7f0f08859c041df
Ketaki Deshwandikar <2106ketaki@gmail.com>
2026-06-15 09:02:40 +0530

    Update config.js

--- streamlibs/utils/config.js
+++ streamlibs/utils/config.js
@@ -40,7 +40,7 @@ export const CONFIG = {
   },
   dev: {
     streamMapper: {
-      serviceEP: 'http://localhost:8082',
+      serviceEP: 'https://adobe-acom-stream-service-deploy-ethos502-prod-or2-1de07c.cloud.adobe.io',
       figmaMappingUrl: '/api/fig-comps',
       figmaBlockContentUrl: '/api/fig-comp-details',
       pushToDaUrl: '/api/push-html',
@@ -54,7 +54,7 @@ export const CONFIG = {
   },
   dev02: {
     streamMapper: {
-      serviceEP: 'http://localhost:8082',
+      serviceEP: 'https://adobe-acom-stream-service-deploy-ethos501-prod-or2-b0c6b7.cloud.adobe.io',
       figmaMappingUrl: '/api/fig-comps',
       figmaBlockContentUrl: '/api/fig-comp-details',
       pushToDaUrl: '/api/push-html',
