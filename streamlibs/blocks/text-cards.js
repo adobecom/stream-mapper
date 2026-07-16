4ce7c12ff83813b92fb490905ca93b0bc412775d
Saurabh Sircar <saurabhsircar11@gmail.com>
2026-07-16 20:41:23 +0530

    AI Block Mapping | Editorial cards handle ups with count (#224)
    Handling ups with count also if there is no tag present. Ups on basis on block count.

--- streamlibs/blocks/text-cards.js
+++ streamlibs/blocks/text-cards.js
@@ -51,7 +51,12 @@ export default async function mapBlockContent(sectionWrapper, blockContent, figC
     });
     blockContent.classList.add('to-remove');
     sectionWrapper.querySelectorAll('.to-remove').forEach((el) => el.remove());
-    handleUpsWithSectionMetadata(sectionWrapper, blockContent, properties.miloTag.toLowerCase());
+    handleUpsWithSectionMetadata(
+      sectionWrapper,
+      blockContent,
+      properties.miloTag.toLowerCase(),
+      properties.blocks?.length,
+    );
     if (properties.background) {
       handleBackgroundWithSectionMetadata(sectionWrapper, blockContent, properties.background);
     }
